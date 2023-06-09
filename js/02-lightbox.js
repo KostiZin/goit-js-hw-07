b495f68ed23e6379f0074f93aaa79fb49334472f
KostiZin <pancostian@gmail.com>
2023-06-09 18:30:47 -0400

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector(`.gallery`);
+
+const markupLB = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+     <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`
+);
+
+function handleShow(evt) {
+  evt.preventDefault();
+
+  const { target } = evt;
+
+  const liData = target.dataset.source;
+  const currentItem = galleryItems.find(({ original }) => original === liData);
+
+  let lightbox = new SimpleLightbox(`gallery a`, {
+    captions: true,
+    captionSelector: `alt`,
+    captionDelay: 250,
+  });
+
+  const instance = ulEl.create(
+    `<a href="${currentItem.original}"><img src="${currentItem.preview}" alt="${currentItem.description}" title="${currentItem.description}"/></a>`
+  );
+
+  instance.show();
+
+  ulEl.addEventListener(`keydown`, handleClose);
+
+  function handleClose(evt) {
+    if (evt.keyCode === 27) {
+      instance.close();
+    }
+  }
+}
+
+ulEl.insertAdjacentHTML(`beforeend`, markupLB.join(``));
+
+ulEl.addEventListener(`click`, handleShow);
