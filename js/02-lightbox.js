aa3cf4d110c1f19ac66bbe0c51cafc824056478c
KostiZin <pancostian@gmail.com>
2023-06-11 10:18:14 -0400

    Update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,46 +1,32 @@
 import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 const ulEl = document.querySelector(`.gallery`);
 
-const markupLB = galleryItems.map(
-  ({ preview, original, description }) =>
-    `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-     <img class="gallery__image" src="${preview}" alt="${description}" />
-  </a>
+// Create a new markup
+
+const markup = galleryItems.map(
+  ({ preview, original, description }) => `<li class="gallery__item">
+ <a class="gallery__link" href="${original}">
+ <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
 </li>`
 );
 
+ulEl.insertAdjacentHTML(`beforeend`, markup.join(``));
+
+// // Create a function from the eventListener
+
 function handleShow(evt) {
   evt.preventDefault();
-
-  const { target } = evt;
-
-  const liData = target.dataset.source;
-  const currentItem = galleryItems.find(({ original }) => original === liData);
-
-  let lightbox = new SimpleLightbox(`gallery a`, {
-    captions: true,
-    captionSelector: `alt`,
-    captionDelay: 250,
-  });
-
-  const instance = ulEl.create(
-    `<a href="${currentItem.original}"><img src="${currentItem.preview}" alt="${currentItem.description}" title="${currentItem.description}"/></a>`
-  );
-
-  instance.show();
-
-  ulEl.addEventListener(`keydown`, handleClose);
-
-  function handleClose(evt) {
-    if (evt.keyCode === 27) {
-      instance.close();
-    }
-  }
 }
 
-ulEl.insertAdjacentHTML(`beforeend`, markupLB.join(``));
-
 ulEl.addEventListener(`click`, handleShow);
+
+// Add settings from the SimpleLightbox
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
